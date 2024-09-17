f7e69eba5749bbc24c4dad6bb81d0343f78e4697
LeeDongRyul57 <ga0212kimi@naver.com>
2024-09-17 23:11:03 +0900

    first commit
    처음 올리는 코드!

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+//HELLO CPP
+
+#include <iostream> //입출력 라이브러리
+#define num 45
+#define square(x) (x*x)
+
+int main(){
+    int a,b,c;
+    std::cout<<"hello wolrd "<<square(num)<<std::endl;
+    std::cin>>a;
+    std::cin>>b;
+    c=a+b;
+    std::cout<<"결과 값은 "<<(7*3/4-2)*5<<std::endl;
+    return 0;
+}
